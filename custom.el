6b4dbfeb6015d5889978434094396c22faa1f278
Tom Olsson <mail@tomolsson.se>
2016-01-19 10:27:18 +0100

    custom.el latex heading color

--- custom.el
+++ custom.el
@@ -11,5 +11,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-latex-sectioning-3-face ((t (:inherit font-latex-sectioning-4-face :foreground "color-33" :height 1.1))))
  '(font-latex-sectioning-5-face ((t (:foreground "color-20" :weight bold))))
  '(font-lock-negation-char-face ((t (:foreground "red")))))
