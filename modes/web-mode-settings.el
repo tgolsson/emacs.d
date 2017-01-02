28dde467196e48d354f264596bc13265d897c1d3
Tom Olsson <mail@tomolsson.se>
2017-01-02 13:08:08 +0100

    indent-size 2->4

--- modes/web-mode-settings.el
+++ modes/web-mode-settings.el
@@ -23,7 +23,7 @@
   ;; set indentation, can set different indentation level for different code type
   (setq web-mode-code-indent-offset 4)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-markup-indent-offset 4)
 
   
 )
