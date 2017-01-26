d4722820f26854595239f98136ae341a99828048
Olsson <tom.olsson@dice.se>
2017-01-26 14:30:04 +0100

    Set git path xor load mu4e if windows/linux

--- settings/settings.el
+++ settings/settings.el
@@ -1,5 +1,26 @@
 ;; IDO-mode
 
+
+;; Set Windows-specific preferences if running in a Windows environment.
+(defun udf-windows-setup () (interactive)
+  ;; The variable `git-shell-path' contains the path to the `Git\bin'
+  ;; file on my system. I install this in
+  ;; `%USERPROFILE%\LocalAppInfo\apps\Git\bin'.
+  (setq git-shell-path
+        (concat "C:\\Program Files\\Git\\bin"))
+  (setq git-shell-executable
+        (concat git-shell-path "\\bash.exe"))
+  (add-to-list 'exec-path git-shell-path)
+  (setenv "PATH"
+          (concat git-shell-path ";"
+                  (getenv "PATH")))
+  (message "Windows preferences set."))
+
+(if (eq system-type 'windows-nt)
+    (udf-windows-setup)
+  (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e"))
+	
+	
 ;; Commands
 (autoload 'zap-up-to-char "misc"
   "Kill up to, but not including ARGth occurrence of CHAR." t)
