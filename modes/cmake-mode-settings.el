7cf0c856386711967f05b54cf4470161b7d8fa8b
Tom Olsson <mail@tomolsson.se>
2017-01-29 20:38:17 +0100

    Switch to usepackage
    Switch arduino-mode, cc-mode, cmake-mode, doc-view-mode, elisp-mode,
    javascript-mode, lua-mode and markdown mode to use-package.

--- modes/cmake-mode-settings.el
+++ modes/cmake-mode-settings.el
@@ -1,12 +1,12 @@
-(require 'company-cmake)
-(require 'company-yasnippet)
-(require 'cmake-font-lock)
-(defun my-cmake-settings ()
-  "Sets up environment for cmake"
-  (make-local-variable 'company-backends)
-  (setq company-backends '(company-cmake company-yasnippet))
-  (company-mode 1)
-  (cmake-font-lock-activate)
-  )
-
-(add-hook 'cmake-mode-hook 'my-cmake-settings)
+(use-package cmake-mode
+  :mode (("/CMakeLists\\.txt\\'" . cmake-mode)
+	 ("\\.cmake\\'" . cmake-mode))
+  :config
+  (use-package company-cmake)
+  (use-package cmake-font-lock)
+  :init
+  (add-hook 'cmake-mode-hook '(lambda () (interactive "")
+                                (make-local-variable 'company-backends)
+                                (setq company-backends '(company-cmake company-yasnippet))
+                                (company-mode 1)
+                                (cmake-font-lock-activate))))
