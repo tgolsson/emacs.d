013a3af1780906aaaae63442f241c4dfe3151f2c
Tom Olsson <mail@tomolsson.se>
2017-02-10 16:39:24 +0100

    Add visual line mode to Darkroom

--- settings/darkroom-setup.el
+++ settings/darkroom-setup.el
@@ -1,10 +1,12 @@
 (use-package darkroom
+  :ensure t
   :config
   (add-hook 'darkroom-tentative-mode-hook (lambda () (progn 
                                                        (if darkroom-tentative-mode 
                                                            (progn
                                                              (linum-mode 0)
-                                                             (darkroom--set-margins))
+                                                             (darkroom--set-margins)
+                                                             (visual-line-mode 1))
                                                          (linum-mode 1)))))
   (setq darkroom-margins 0.25))
 
