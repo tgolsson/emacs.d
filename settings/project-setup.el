88b0e283c022245fbbcab09ae21e4b95de8788e4
Tom Olsson <mail@tomolsson.se>
2017-02-17 17:39:09 +0100

    Remove neotree
    Neotree is mostly in the way so it was removed. Projectile-switch-action
    now uses magit-status instead.

--- settings/project-setup.el
+++ settings/project-setup.el
@@ -1,14 +1,10 @@
 
-(use-package neotree
-  :bind ("<f8>" . neotree-toggle)
-  :config 
-  (setq neo-window-position 'right))
 
 (use-package projectile
   :bind ("C-x M-k" . to/kill-other-buffers)
   :config 
   (projectile-global-mode 1)
-  (setq projectile-switch-project-action 'neotree-projectile-action))
+  (setq projectile-switch-project-action 'magit-status)
 (use-package ibuffer-projectile)
 
 (provide 'project-setup)
