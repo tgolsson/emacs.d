6a2bc14bec07a994848fdd6e12449a2ec8b1ecf6
Tom Olsson <tom.olsson@embark-studios.com>
2019-05-08 15:54:04 +0200

    [defuns] add function to toggle underscores and camelcase

--- settings/defuns.el
+++ settings/defuns.el
@@ -306,4 +306,21 @@ Note: this code uses https://en.wikipedia.org/wiki/Md5"
    ;; generate for emacs
    "emacs"))
 
+(defun toggle-camelcase-underscores ()
+  "Toggle between camelcase and underscore notation for the symbol at point."
+  (interactive)
+  (save-excursion
+    (let* ((bounds (bounds-of-thing-at-point 'symbol))
+           (start (car bounds))
+           (end (cdr bounds))
+           (currently-using-underscores-p (progn (goto-char start)
+                                                 (re-search-forward "_" end t))))
+      (if currently-using-underscores-p
+          (progn
+            (upcase-initials-region start end)
+            (replace-string "_" "" nil start end)
+            (downcase-region start (1+ start)))
+        (replace-regexp "\\([A-Z]\\)" "_\\1" nil (1+ start) end)
+        (downcase-region start (cdr (bounds-of-thing-at-point 'symbol)))))))
+
 (provide 'defuns)
