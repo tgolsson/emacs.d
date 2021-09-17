8d45de4b705789c92131c0ba0e9527b5aefb472c
Tom Solberg <tom.solberg@embark-studios.com>
2021-09-17 23:37:42 +0200

    tune go-mode

--- modes/my-go-mode.el
+++ modes/my-go-mode.el
@@ -1,15 +1,12 @@
 (use-package go-mode
   :mode "\\.go\\'"
   :init
-  (use-package company-go :hook go-mode)
-  (use-package flycheck-golangci-lint :hook go-mode)
-  (use-package go-eldoc :hook go-mode)
-  (use-package go-gopath :hook go-mode)
-  (use-package go-impl :hook go-mode)
-  (use-package godoctor :hook go-mode)
-  (use-package go-projectile :commands go-projectile-tools-add-path :hook go-mode)
-  :init
-  ;; (eval-after-load "lsp-mode" '(puthash "GOPROXY" "https://proxy.golang.org,direct" lsp-go-env))
+  ;; (use-package company-go :hook go-mode)
+  (use-package flycheck-golangci-lint :commands flycheck-golangci-lint-setup)
+  ;; (use-package go-eldoc :hook go-mode)
+  ;; (use-package go-gopath :hook go-mode)
+  ;; (use-package go-impl :hook go-mode)
+  (use-package go-projectile :commands go-projectile-tools-add-path :hook (go-mode . go-projectile-set-gopath))
   :config
   (defun to/my-go-mode ()
     ;; (define-key go-mode-map (kbd "C-c C-e") #'go-gopath-set-gopath)
@@ -21,5 +18,9 @@
     (set (make-local-variable 'company-backends) '(company-capf))
     (lsp)
     (lsp-mode 1)
+    (flycheck-golangci-lint-setup)
+    (flycheck-mode 1)
+    (flycheck-pos-tip-mode 1)
+    (flycheck-add-next-checker 'lsp 'golangci-lint)
     (add-hook 'before-save-hook 'gofmt-before-save nil t))
   (add-hook 'go-mode-hook 'to/my-go-mode))
